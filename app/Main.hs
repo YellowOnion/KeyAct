e32548c3f4c21c8b63b487266abd2af8a2b2134a
Daniel Hill <daniel@gluo.nz>
2024-08-23 15:25:30 +1200

    Add mouse support, refactor to help.

--- app/Main.hs
+++ app/Main.hs
@@ -16,15 +16,18 @@ import qualified Network.WebSockets as WS
 
 import qualified Evdev as Ev
 import qualified Evdev.Codes as EvCodes
+import qualified Evdev.Codes as Ev
+
+import Debug.Trace (traceShow)
 
 keys = [ EvCodes.KeyQ, EvCodes.KeyW, EvCodes.KeyE, EvCodes.KeyA, EvCodes.KeyS
-       , EvCodes.KeyD, EvCodes.KeyF, EvCodes.KeySpace ]
+       , EvCodes.KeyD, EvCodes.KeyF, EvCodes.KeySpace, EvCodes.BtnLeft, EvCodes.BtnRight, EvCodes.BtnMiddle ]
 
 evToText :: Ev.EventData -> Maybe String
 evToText (Ev.KeyEvent key state)
   | key `elem` keys && state /= Ev.Repeated = Just $ show key ++ "::" ++ show state
   | otherwise = Nothing
-evToText _ = Nothing
+evToText e = Nothing
 
 parseEvent :: Ev.Event -> Maybe String
 parseEvent (Ev.Event evData _) = evToText evData
@@ -32,8 +35,10 @@ parseEvent (Ev.Event evData _) = evToText evData
 main :: IO ()
 main = do
   args <- Env.getArgs
-  fp <- BS.fromFilePath $ args !! 0
-  BS.putStrLn $ "Opening device at: " <> fp
+  kb <- BS.fromFilePath $ args !! 0
+  mouse <- BS.fromFilePath $ args !! 1
+  BS.putStrLn $ "Opening device at: " <> kb
+  BS.putStrLn $ "Opening device at: " <> mouse
   outputChan <- atomically $ newBroadcastTChan :: IO (TChan String)
 
   forkIO . WS.runServer ("127.0.0.1") 9160
@@ -49,9 +54,14 @@ main = do
           msg <- atomically $ readTChan chan
           msg `seq` WS.sendTextData conn $ T.pack msg
 
-  dev <- Ev.newDevice fp
-  _ :: () <- forever $  do
-    event <- Ev.nextEvent dev
+  devKb <- Ev.newDevice kb
+  forkIO . forever $  do
+    event <- Ev.nextEvent devKb
+    forM_ (parseEvent event) (atomically . writeTChan outputChan)
+
+  devMouse <- Ev.newDevice mouse
+  forkIO . forever $  do
+    event <- Ev.nextEvent devMouse
     forM_ (parseEvent event) (atomically . writeTChan outputChan)
 
   _ <- getLine
